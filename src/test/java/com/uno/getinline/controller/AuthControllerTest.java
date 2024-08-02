ffa9dc3bf56df1f70a22aa7e619b42dbfe36dd8d
LEEDAEJIN <ldj4241@naver.com>
2024-08-02 13:20:03 +0900

    Apply - Spring Security 시큐리티 기능 적용 및 기존 테스트는 시큐리티와 별개로 동작시키고 싶으므로 시큐리티 자동 설정과 필터를 제외하는 규칙을 추가 인증 테스트는 필요에 따라 별도로 진행

--- src/test/java/com/uno/getinline/controller/AuthControllerTest.java
+++ src/test/java/com/uno/getinline/controller/AuthControllerTest.java
@@ -1,9 +1,13 @@
 package com.uno.getinline.controller;
 
+import com.uno.getinline.config.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -11,8 +15,12 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(AuthController.class)
-class AuthControllerTest {
+@WebMvcTest(
+        controllers = AuthController.class,
+        excludeAutoConfiguration = SecurityAutoConfiguration.class,
+        excludeFilters = @ComponentScan.Filter(type=
+                FilterType.ASSIGNABLE_TYPE,classes = SecurityConfig.class)
+)class AuthControllerTest {
 
     private final MockMvc mvc;
 
--- src/test/java/com/uno/getinline/controller/error/BaseErrorControllerTest.java
+++ src/test/java/com/uno/getinline/controller/error/BaseErrorControllerTest.java
@@ -1,17 +1,26 @@
 package com.uno.getinline.controller.error;
 
+import com.uno.getinline.config.SecurityConfig;
+import com.uno.getinline.controller.AdminController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(BaseErrorController.class)
-class BaseErrorControllerTest {
+@WebMvcTest(
+        controllers = BaseErrorController.class,
+        excludeAutoConfiguration = SecurityAutoConfiguration.class,
+        excludeFilters = @ComponentScan.Filter(type=
+                FilterType.ASSIGNABLE_TYPE,classes = SecurityConfig.class)
+)class BaseErrorControllerTest {
 
     private final MockMvc mvc;
 
